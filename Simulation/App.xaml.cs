da8bdc2fe5303ca39dc70a16a31b205638c4e9eb
red-eyed-99 <mgnblack99@gmail.com>
2024-07-23 14:36:51 +0300

    Добавьте файлы проекта.

---
+++ Simulation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Simulation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
